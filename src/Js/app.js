854beeef1945f9c1895de3f077af9d1f5cf7e361
Akbar Sharofiddinov <akbarsharofiddinov@gmail.com>
2024-04-23 14:43:44 +0500

    qurilish-html&css&js

---
+++ src/Js/app.js
@@ -0,0 +1,141 @@
+const headerTop = document.querySelector(".header-top");
+const header = document.querySelector(".header");
+const mainElement = document.querySelector(".main");
+const toTopBtn = document.querySelector(".toTop-btn");
+
+AOS.init();
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= 100) {
+    headerTop.style.transform = "translateY(-100px)";
+    header.style.marginTop = 0;
+  } else {
+    headerTop.style.transform = "translateY(0)";
+    header.style.marginTop = `${headerTop.getBoundingClientRect().height}px`;
+  }
+});
+
+header.style.marginTop = `${headerTop.getBoundingClientRect().height}px`;
+mainElement.style.marginTop = `${
+  headerTop.getBoundingClientRect().height +
+  header.getBoundingClientRect().height
+}px`;
+
+const maps = document.querySelectorAll(".jqvmap-region");
+maps.forEach((regionMap, index) => {
+  // Hover Effect
+  regionMap.addEventListener("mouseover", () => {
+    regionMap.setAttribute("fill", "#6c7ba3");
+  });
+
+  regionMap.addEventListener("mouseleave", () => {
+    regionMap.setAttribute("fill", regionMap.getAttribute("original"));
+  });
+  // ==============================
+
+  // Selecting
+  regionMap.addEventListener("click", () => {
+    const currentRegion = regionMap.id.split("_");
+    document.querySelector(".areaTitle").innerHTML =
+      currentRegion[currentRegion.length - 1];
+
+    regionMap.classList.add("active");
+    maps.forEach((element, elementID) => {
+      if (elementID !== index) element.classList.remove("active");
+    });
+  });
+});
+
+// ====================================================================
+// ====================================================================
+
+// Accordion configuration
+
+const accordionBtns = document.querySelectorAll(".accordionBtn");
+accordionBtns.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("active");
+    document.querySelectorAll(".content")[index].classList.toggle("active");
+  });
+});
+
+// Statistics
+const doughnut = document.getElementById("myChart");
+const renderVals = document.createElement("div");
+
+const vals = [
+  { title: "Ko'p qavatli uylar", value: 40789 },
+  { title: "Boshqaruv kompaniyalar soni", value: 812 },
+  { title: "Shirkatlar soni", value: 117 },
+];
+const backgroundColors = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+];
+
+new Chart(doughnut, {
+  type: "doughnut",
+  data: {
+    labels: vals.map((item) => item.title),
+    datasets: [
+      {
+        label: ["# value"],
+        data: vals.map((item) => item.value),
+        borderWidth: 0,
+        backgroundColor: backgroundColors,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+renderVals.innerHTML = `
+  <p>
+    <span>${vals[0].title}</span>
+    <span style="color: ${backgroundColors[0]}">${vals[0].value}</span>
+  </p>
+  <p>
+    <span>${vals[1].title}</span>
+    <span style="color: ${backgroundColors[1]}">${vals[1].value}</span>
+  </p>
+  <p>
+    <span>${vals[2].title}</span>
+    <span style="color: ${backgroundColors[2]}">${vals[2].value}</span>
+  </p>
+`;
+renderVals.classList.add("renderVals");
+
+document.getElementById("chartVals").appendChild(renderVals);
+
+// ===========================================
+// Swiper Js
+
+var swiper = new Swiper(".resourceSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// To Top button
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= 200) {
+    toTopBtn.classList.add("active");
+  } else {
+    toTopBtn.classList.remove("active");
+  }
+});
---
+++ src/Js/components/header.js
@@ -0,0 +1,42 @@
+export const headerTop = `<div class="header-top">
+<div class="container">
+  <div class="headerTop-inner">
+    <ul class="social-links">
+      <li class="social-link"><i class="fa-brands fa-instagram"></i></li>
+      <li class="social-link"><i class="fa-brands fa-facebook"></i></li>
+      <li class="social-link"><i class="fa-brands fa-youtube"></i></li>
+      <li class="social-link"><i class="fa-brands fa-telegram"></i></li>
+    </ul>
+    <div class="topInner">
+      <div class="phone">
+        <i class="fa-solid fa-phone-volume"></i>
+        <div>
+          <p>Ishonch telefoni</p>
+          <p>1298</p>
+        </div>
+      </div>
+      <div class="email">
+        <i class="fa-regular fa-envelope"></i>
+        <div>
+          <p>Elektron manzil</p>
+          <p>info@mc.uz</p>
+        </div>
+      </div>
+      <div class="top-tools">
+        <div class="search">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="specialView">
+          <i class="fa-regular fa-eye"></i>
+        </div>
+        <div class="languages">
+          <i class="fa-solid fa-globe"></i>
+          <span>O'Z</span>
+          <span>ЎЗ</span>
+          <span>РУ</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`;
